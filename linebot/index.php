6f386589d948123c96382b627fa3ce25b6aad7cc
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-10-27 01:43:26 +0700

    start lineboxt

---
+++ linebot/index.php
@@ -0,0 +1,11 @@
+<?php error_reporting(E_ALL ^ E_NOTICE);
+/*****************************************************************
+Created :26/10/2021
+Author : worapot bhilarbutra (pros.ake)
+E-mail : worapot.bhi@gmail.com
+Website : https://www.vpslive.com
+Copyright (C) 2021-2025, VPS Live Digital togethers all rights reserved.
+ *****************************************************************/
+include_once("../include/config.inc.php");
+
+echo "Hello Panjaree";
