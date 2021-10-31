ff157ed7aa4ea4329d23292e344e27e5c5b7afe7
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-11-01 02:14:41 +0700

    text check

--- linebot/index.php
+++ linebot/index.php
@@ -51,7 +51,10 @@ if ($pos > 0) {
 //$messages = [];
 $messages['replyToken'] = $replyToken;
 $messages['messages'][0] = getFormatTextMessage("คุณ " . $userId . " ข้อความ =>" . $text);
-$messages['messages'][1] = getFormatTextMessage("รหัสนี้เป็นของ =>" . $textcut);
+$messages['messages'][1] = getFormatTextMessage("รหัสนี้เป็นของ =>" . $deliver);
+$messages['messages'][1] = getFormatTextMessage("\n รหัสนี้ยาว =>" . $textlen)
+$messages['messages'][1] = getFormatTextMessage("\n รหัสนี้ตำแหน่ง =>" . $textpos)
+
 //$messages['messages'][0] = getFormatTextMessage("นี่ลิ้งติดตาม https://www.flashexpress.co.th/tracking/?se=" . $text);
 /*
 $datas = [];
