2f5aeebaefb26637021f609145723442d9d6fbef
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-11-04 02:46:45 +0700

    update

--- flashexpress.php
+++ flashexpress.php
@@ -1,6 +1,7 @@
+
 <?php
 header('Content-Type:application/json');
-$url = "https://www.flashexpress.co.th/tracking/?se=TH340120CJA04B";
+$url = "https://www.flashexpress.co.th/tracking/?se=TH273221W9AN2D";
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -8,13 +9,6 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 $res = curl_exec($ch);
 curl_close($ch);
 
+echo $res;
 
-$dom = new DomDocument();
-@$dom->loadHTML($res);
-
-
-$classname = "res-info-div res-info-div-1";
-$finder = new DomXPath($dom);
-$div = $finder->query("//div[@class="head"]");
-$val = $div->item(0)->textContent;
-echo $title . ' ' . substr($val, 0) . '<br>';
+?>
