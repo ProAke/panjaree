129b8a22117f1d22c1cba2fc3aba488bf9d9287b
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-11-04 22:29:34 +0700

    Dashboard
    web

---
+++ jqphp/task-list.php
@@ -0,0 +1,14 @@
+<?php error_reporting(E_ALL ^ E_NOTICE);
+/*****************************************************************
+Created		: 21/10/2021
+Author		: Ake.Worapot
+E-mail		: worapot@yahoo.com
+server		: www.vpslive.com
+PHP Version : 7.0++
+Copyright (C) 2010-2025, VPSLive Digital Together's , all rights reserved.
+ *****************************************************************/
+
+include_once("../include/config.inc.php");
+
+
+// Select Data
