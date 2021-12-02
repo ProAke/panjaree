7c40d5742fbf24015f81380a13bd8bed9bb77f7f
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-12-02 19:56:54 +0700

    Update pos_auto.php
    12

--- express/pos_auto.php
+++ express/pos_auto.php
@@ -1,52 +1,50 @@
 <?php
-//include_once("../include/config.inc.php");
-//include_once("../include/class.inc.php");
-//include_once("../include/function.inc.php");
-
-//header('Content-Type: application/json; charset=utf-8');
+/***************************************************
+ * Author    : CS Developers
+ * Author URI: https://www.comscidev.com
+ * Facebook  : https://www.facebook.com/CSDevelopers
+ ***************************************************/
+ 
 $api_token_url = 'https://trackapi.thailandpost.co.th/post/api/v1/authenticate/token';
 $api_track_url = 'https://trackapi.thailandpost.co.th/post/api/v1/track';
-$token_key     = 'OuTlWpIqYmZKHUU:TBZ9SLT?D!KgX.FOSMAjMmCwGMK4YdKJNqX9V6SUC-K-X2IfC-NmR6DUH=SYTmBAV0SGJoCxGnCnBbNWLCHZ';
+$token_key     = 'DiUdCxHtZlFcFcS^DpV9XJPvD$N:BSC$YzDhNvS2GMF2NDOgT5C*KoJZDmQEVKS;UGHgVTZRS%M&CcB7CjYrXPBRGZS-KBZIPJSM';
 
-
-
-$trackid = $_GET['tk'];
-
-
-function api_request($url, $token, $content = null)
-{
-
-    $headers = array(
-        'Authorization: Token ' . $token,
+ 
+function api_request($url, $token, $content = null){
+     
+    $headers = [
+        'Authorization: Token '. $token,
         'Content-Type: application/json'
-    );
-
+    ];
+     
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($content));
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt( $ch, CURLOPT_URL, $url );
+    curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($content) );
+    curl_setopt( $ch, CURLOPT_POST, true );
+    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    $result = curl_exec($ch);
+ 
+    $result = curl_exec( $ch );
     curl_close($ch);
-
-    return json_decode($result, true);
+       
+    return json_decode($result, true);  
 }
-
-$items = "[
-'status' => 'all',
-'language' => 'TH',
-'barcode' => ['EG955963096TH'] // บันทัดนี้ barcode มันต้องเป็น array จริงหรือเปล่า
-]";
-
-
-echo $items . "<br>\n";
-
+ 
+//Items
+$items = [
+    'status' => 'all',
+    'language' => 'TH',
+    'barcode' => [
+        'ED852942182TH'
+    ]
+];
+ 
+//Step1: GetToken()
 $res_token = api_request($api_token_url, $token_key);
-echo $res_token . "-2<br>\n";
-$res_items = api_request($api_track_url, $res_token['token'], json_encode($items));
-echo $res_items . "-3<br>\n";
-
-echo "--------------------";
+ 
+ 
+//Step2: GetItems()
+$res_items = api_request($api_track_url, $res_token['token'], $items);
+ 
+print_r($res_items);
