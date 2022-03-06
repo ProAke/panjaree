41499a89bd50fd8e5ce47045846dc13868563b64
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2022-03-06 21:11:39 +0700

    Update callback.php

--- line/callback.php
+++ line/callback.php
@@ -149,5 +149,5 @@ if (isset($res->error)) {
     echo $strSql . "<br>";
     echo $userId;
 
-    echo '<meta http-equiv="Refresh" content="20;https://panjaree.uarea.in' . $state . '">';
+    echo '<meta http-equiv="Refresh" content="20;https://panjaree.uarea.in/authentication/index.php?LineID=' . $userId . '">';
 }
