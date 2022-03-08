3485eb9f20109342a1a1d8383474838acada1bf9
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2022-03-09 01:07:09 +0700

    session login

--- check/index.php
+++ check/index.php
@@ -12,17 +12,7 @@ include_once("../include/config.inc.php");
 include_once("../include/class.inc.php");
 include_once("../include/class.TemplatePower.inc.php");
 include_once("../include/function.inc.php");
-//include_once("../authentication/check_login.php");
+include_once("../authentication/check_login.php");
 
-echo $_SESSION['LineID'] . "<<<";
-echo $_SESSION['UserID'] . "<<<";
-
-$_SESSION = array();
-
-print_r($_COOKIE);  // แสดง Cookies ทั้งหมดที่ php สามารถอ่านได้ 
-// สร้าง Loop เพื่อ กำหนดให้ Cookies หมดอายุไป
-
-foreach ($_COOKIE as $key => $value) {
-    unset($value);
-    setcookie($key, '', time() - 3600);
-}
+echo $_SESSION['LineID'];
+echo $_SESSION['UserID'];
