24283e1abc922f3e3aa60bc1492df669f2e79454
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2022-03-09 01:43:20 +0700

    all update check login

--- products/all.php
+++ products/all.php
@@ -13,6 +13,8 @@ include_once("../include/config.inc.php");
 include_once("../include/class.inc.php");
 include_once("../include/class.TemplatePower.inc.php");
 include_once("../include/function.inc.php");
+include_once("../authentication/check_login.php");
+
 
 $tpl = new TemplatePower("../template/_tp_inner.html");
 $tpl->assignInclude("body", "_tp_all.html");
--- products/crud.php
+++ products/crud.php
@@ -12,7 +12,7 @@ include_once("../include/config.inc.php");
 include_once("../include/class.inc.php");
 include_once("../include/class.TemplatePower.inc.php");
 include_once("../include/function.inc.php");
-
+include_once("../authentication/check_login.php");
 
 
 
