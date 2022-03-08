84d125e33d0b8583398ed69b581553f8f2892190
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2022-03-09 00:55:37 +0700

    check update

--- check/index.php
+++ check/index.php
@@ -21,9 +21,9 @@ echo $_SESSION['UserID'] . "<<<";
 
 print_r($_COOKIE);  // แสดง Cookies ทั้งหมดที่ php สามารถอ่านได้ 
 // สร้าง Loop เพื่อ กำหนดให้ Cookies หมดอายุไป
-/*
-foreach($_COOKIE as $k=>$v) {
-	setcookie($k, "", time() - 3600); 
+
+foreach ($_COOKIE as $k => $v) {
+    setcookie($k, "", time() - 3600);
 }
 
-*/
+$_SESSION = array();
